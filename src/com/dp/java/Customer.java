58d14b7e345b5542bf3c562f43cccaa38b2ef867
dp <dpprasad009@gmail.com>
2024-06-17 12:58:52 +0530

    Initial commit

---
+++ src/com/dp/java/Customer.java
@@ -0,0 +1,19 @@
+package com.dp.java;
+
+public class Customer
+{
+    public static void main(String[] args) {
+        /*
+        working with data types
+         */
+        byte age;
+        age=24;
+        System.out.println("Age: " + age);
+
+        short orderValue;
+        orderValue =3456;
+        System.out.println("orderValue: " + orderValue);
+
+
+    }
+}
